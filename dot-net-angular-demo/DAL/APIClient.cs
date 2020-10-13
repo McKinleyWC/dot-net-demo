355827df1d2cd81242f39ba8ff80121950b36fd4
Will McKinley <mckinleywc@gmail.com>
2020-10-12 23:48:12 -0400

    Added DAL APIClient to get JSON Payload AnnualProdReport Class to deserialize JSON

---
+++ dot-net-angular-demo/DAL/APIClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace DAL
+{
+    public class APIClient
+    {
+        public static List<AnnualProdReport> GetAnnualProdReportFromAPI(string baseUri, string requestUri)
+        {
+            var theReports = new List<AnnualProdReport>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://inventory.data.gov/api/3/");
+                //HTTP GET
+                var responseTask = client.GetAsync("action/datastore_search?resource_id=72a33ebb-3efe-455b-9e41-0733aaed7780");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    var readTask = result.Content.ReadAsAsync<List<AnnualProdReport>>();
+                    readTask.Wait();
+
+                    theReports = readTask.Result;
+                }
+            }
+            return theReports;
+        }
+    }
+}
---
+++ dot-net-angular-demo/DAL/AnnualProdReport.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace DAL
+{
+    public class AnnualProdReport
+    {
+        public string resource_id { get; set; }
+        public List<Field> fields { get; set; }
+        public List<Record> records { get; set; }
+        public _Links _links { get; set; }
+        public int limit { get; set; }
+        public int total { get; set; }
+    }
+
+    public class _Links
+    {
+        public string start { get; set; }
+        public string next { get; set; }
+    }
+
+    public class Field
+    {
+        public string type { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Record
+    {
+        public string _5Bills { get; set; }
+        public string _50Bills { get; set; }
+        public string _10Bills { get; set; }
+        public string FiscalYear { get; set; }
+        public string _1Bills { get; set; }
+        public string _20Bills { get; set; }
+        public int _id { get; set; }
+        public string _2Bills { get; set; }
+        public string _100Bills { get; set; }
+    }
+}
